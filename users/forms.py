ed5168b79ad30c93c57b0d48c45dbcb3a3c91de5
FailCrew <samostroenkod@gmail.com>
2019-05-24 17:45:09 +0800

    add styles to SignUp form

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,62 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    city = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    school = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    clas = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    team = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    lastname = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email', 'city', 'school', 'clas', 'team', 'lastname', 'name')
+        
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'city', 'school', 'clas', 'team', 'lastname', 'name')
+
+class SignUpForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    
